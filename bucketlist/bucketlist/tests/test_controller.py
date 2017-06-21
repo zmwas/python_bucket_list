4c4775c2c1ded61932abcf48ceb52c47fa15d00b
zac234 <zmwangimk@gmail.com>
2017-06-21 15:45:11 +0300

    Add test for login

--- bucketlist/bucketlist/tests/test_controller.py
+++ bucketlist/bucketlist/tests/test_controller.py
@@ -1,17 +1,26 @@
 import unittest
-from accounts.bucket_user import BucketUser
-from user_controller import list_of_users, UserAuth
+from user_controller import UserAuth
 
 
 class TestController(unittest.TestCase):
     def setUp(self):
         self.auth = UserAuth()
-        self.user = BucketUser()
 
     def test_user_registration_successful(self):
         """
         Tests successful user registration
 
         """
-        user = BucketUser()
-        self.assertEqual(len(self.auth.add_user("Zack","zacmwanginj@gmail.com","Hunter1")), 1, msg="Method should add user to list")
+        # self.assertEqual(len(self.auth.add_user("Zack", "zacmwanginj@gmail.com", "Hunter1")), 1,
+        #                  msg="Method should add user to list")
+        self.assertTrue(len(self.auth.list_of_users) < len(self.auth.add_user("Zack", "johndoe@gmail.com", "Hunter1")))
+    def test_user_email_exists(self):
+        self.assertEqual(self.auth.add_user("Identity Thief", "zacmwanginj@gmail.com", "1234"), "That email is taken")
+
+    def test_login(self):
+        self.auth.add_user("Zack", "zacmwanginj@gmail.com", "Hunter1")
+
+        self.assertEqual(self.auth.login("zacmwanginj@gmail.com", "Hunter1"), "Welcome")
+
+    def tearDown(self):
+        del self.auth
