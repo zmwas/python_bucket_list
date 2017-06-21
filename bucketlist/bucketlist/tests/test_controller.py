4764088ac4dc7b2d95835e295ba47c6c22c7edb8
zac234 <zmwangimk@gmail.com>
2017-06-21 12:29:21 +0300

    change user registration test name

--- bucketlist/bucketlist/tests/test_controller.py
+++ bucketlist/bucketlist/tests/test_controller.py
@@ -1,18 +1,19 @@
 import unittest
 from accounts.bucket_user import BucketUser
-from user_controller import list_of_users
+from user_controller import list_of_users, UserAuth
 
 
 class TestController(unittest.TestCase):
     def setUp(self):
-        
-        self.my_list=list_of_users
+        self.my_list = list_of_users
 
-
-    def test_user_added_to_db(self):
+    def test_user_registration_successful(self):
+        """
+        Tests successful user registration
+                 
+        """
         user = BucketUser()
-        user.create_user("Zack","zacmwanginj@gmail.com")
+        user.create_user("Zack", "zacmwanginj@gmail.com")
         user.set_password("Hunter1")
-
-        add_user(user)
-        self.assertEqual(len(add_user),len(self.my_list)+1,msg="Method should add user to list")
+        auth = UserAuth()
+        self.assertEqual(len(auth.add_user(user)), len(self.my_list) + 1, msg="Method should add user to list")
