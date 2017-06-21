2f3d3c3663346bcabe9afe0285ce0da39b197ada
zac234 <zmwangimk@gmail.com>
2017-06-21 11:42:35 +0300

    Add test to check name is provided

--- bucketlist/bucketlist/tests/test_bucket_list_methods.py
+++ bucketlist/bucketlist/tests/test_bucket_list_methods.py
@@ -3,16 +3,22 @@
 """
 import unittest
 from buckets.buckets import BucketList
+
+
 class TestBucket(unittest.TestCase):
     """TestCase class for BucketList
 
     """
 
     def setUp(self):
-        self.my_bucket = BucketList("My resolutions","In Progress")
-
+        self.my_bucket = BucketList("My resolutions", "In Progress")
 
     def test_create_bucket_list_returns_bucket_list(self):
-        bucket = BucketList("","")
-        bucket = bucket.create_bucket_list("Name","Completed")
-        self.assertIsInstance(bucket,BucketList)
+        bucket = BucketList("", "")
+        bucket = bucket.create_bucket_list("Name", "Completed")
+        self.assertIsInstance(bucket, BucketList)
+
+    def test_create_bucket_list_checks_for_name(self):
+        bucket = BucketList("", "")
+        bucket = bucket.create_bucket_list("")
+        self.assertEqual(bucket, "Please provide a name for your bucket list", )
