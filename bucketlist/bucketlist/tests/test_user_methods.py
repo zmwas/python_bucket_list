c11323fa4bcae1f027a268dbc119000153adbdbe
zac234 <zmwangimk@gmail.com>
2017-06-21 08:14:57 +0300

    Add test to check if email is provided

--- bucketlist/bucketlist/tests/test_user_methods.py
+++ bucketlist/bucketlist/tests/test_user_methods.py
@@ -1,8 +1,29 @@
 import unittest
 
+from accounts.bucket_user import BucketUser
 
 
 class UserTestCase(unittest.TestCase):
+    """TestCase for user methods
+
+    """
 
     def setUp(self):
-        self.bucket_user = User('Zacharia Mwangi', 'zacmwanginj@gmail.com')
+        self.bucket_user = BucketUser('Zacharia Mwangi', 'zacmwanginj@gmail.com')
+
+    def test_create_user_returns_user_object(self):
+        """
+        Tests whether the create_user method returns a user
+        :return:
+        """
+
+        user = BucketUser("", "")
+        self.assertIsInstance(user.create_user('Zacharia Mwangi', 'zacmwanginj@gmail.com'), BucketUser)
+
+    def test_create_user_requires_email(self):
+        user = BucketUser("", "")
+        self.assertEqual(user.create_user('Zacharia Mwangi', ''), "Please provide an email")
+
+
+if __name__ == '__main__':
+    unittest.main()
