badfe1fe7bd5add5f28cc2e48575c3f2f949162f
zac234 <zmwangimk@gmail.com>
2017-06-21 08:24:55 +0300

    Add test to check for name

--- bucketlist/bucketlist/tests/test_user_methods.py
+++ bucketlist/bucketlist/tests/test_user_methods.py
@@ -13,16 +13,32 @@ class UserTestCase(unittest.TestCase):
 
     def test_create_user_returns_user_object(self):
         """
-        Tests whether the create_user method returns a user
-        :return:
+        Tests that the create_user method returns a user
+
         """
 
         user = BucketUser("", "")
-        self.assertIsInstance(user.create_user('Zacharia Mwangi', 'zacmwanginj@gmail.com'), BucketUser)
+        self.assertIsInstance(user.create_user('Zacharia Mwangi', 'zacmwanginj@gmail.com'), BucketUser,
+                              msg="create user method should return a user object")
 
     def test_create_user_requires_email(self):
+        """
+        Tests that create_user method checks for email
+
+        """
+
         user = BucketUser("", "")
-        self.assertEqual(user.create_user('Zacharia Mwangi', ''), "Please provide an email")
+        self.assertEqual(user.create_user('Zacharia Mwangi', ''), "Please provide an email",
+                         msg="email should be provided to create user")
+
+    def test_create_user_requires_name(self):
+        """
+        Tests that create_user method checks for name
+
+        """
+        user = BucketUser("", "")
+        self.assertEqual(user.create_user('', 'zacmwanginj@gmail.com'), 'Please provide your name',
+                         msg="name should be provided to create user")
 
 
 if __name__ == '__main__':
