2c2b359e0e6f879609fd7bc52948856399e8248e
zac234 <zmwangimk@gmail.com>
2017-06-22 03:43:02 +0300

    Add method to create a new bucket list item

--- bucketlist/bucketlist/bucket_controller.py
+++ bucketlist/bucketlist/bucket_controller.py
@@ -23,6 +23,18 @@ class BucketController():
         return buckets
 
 
+    def add_bucket_item(self,email,bucket_name,name,completion_status):
+        bucket_item = BucketListItems()
+        bucket_item.create_bucket_items(name,completion_status)
+
+        buckets= self.bucket_list_dictionaries.get(email)
+        for bucket in buckets:
+            if bucket_name == bucket[0]:
+                bucket.append(bucket_item)
+
+
+                return bucket
+
 
 
 
