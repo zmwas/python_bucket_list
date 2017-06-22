826e50b68082a894cfe7947511288cf5e094e7a3
zac234 <zmwangimk@gmail.com>
2017-06-22 14:39:45 +0300

    Add variable to check for logged in user

--- bucketlist/bucketlist/controllers/user_controller.py
+++ bucketlist/bucketlist/controllers/user_controller.py
@@ -4,10 +4,12 @@
 # list where users are added on registration
 
 from accounts.bucket_user import BucketUser
+from controllers.bucket_controller import BucketController
 
 
 class UserAuth():
     list_of_users = []
+    current_user=BucketUser()
 
     def add_user(self, name, email, password):
         users = self.list_of_users
@@ -17,10 +19,14 @@ class UserAuth():
                 # break
                 return "That email is taken"
 
+
         user = BucketUser()
         user.create_user(name, email)
         user.set_password(password)
         users.append(user)
+        bucket_controller = BucketController()
+        directory_entry = bucket_controller.bucket_list_dictionaries[user.email] = []
+
 
         return users
 
@@ -28,9 +34,15 @@ class UserAuth():
     def login(self,email,password):
         users = self.list_of_users
 
-        for user in self.list_of_users:
+        for user in users:
             if email == user.email and password == user.get_password():
+                user.is_logged_in = True
+                self.current_user= user
+                print(self.current_user.__dict__)
+                print(self.current_user.email)
                 return "Welcome"
 
             else:
                 return "Wrong email or password"
+
+
