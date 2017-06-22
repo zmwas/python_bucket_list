6359bf6f6256f2e01a5658aca79b38972a13eb5d
zac234 <zmwangimk@gmail.com>
2017-06-22 14:38:03 +0300

    Add method to view bucket list items

--- bucketlist/bucketlist/controllers/bucket_controller.py
+++ bucketlist/bucketlist/controllers/bucket_controller.py
@@ -5,24 +5,36 @@ from buckets.buckets import BucketList
 class BucketController():
     bucket_list_dictionaries = {}
 
-
-
-    def create_bucket_list(self, email, name, completion_status):
+    def create_bucket(self, email, name, completion_status):
         bucketlist = BucketList()
         buckets = self.bucket_list_dictionaries.get(email)
-        for bucket in buckets:
-            if name == bucket[0]:
-                return "Try another name for your bucket"
+        if buckets:
+            for bucket in buckets:
+                print(bucket)
+                if name == bucket[0]:
+                    return "Try another name for your bucket"
 
-        bucketlist.create_bucket_list(name, completion_status)
-        buckets.append(bucketlist)
+            bucketlist.create_bucket_list(name, completion_status)
+            buckets.append(bucketlist)
+        else:
+            bucketlist.create_bucket_list(name, completion_status)
+            buckets.append(bucketlist)
+
         return buckets
 
     def add_bucket_item(self, email, bucket_name, name, completion_status):
         bucket_item = BucketListItems()
         bucket_item.create_bucket_items(name, completion_status)
         buckets = self.bucket_list_dictionaries.get(email)
+        print(self.bucket_list_dictionaries.__dict__)
         for bucket in buckets:
             if bucket_name == bucket[0]:
                 bucket.append(bucket_item)
                 return bucket
+
+        return bucket
+
+    def view_single_bucket(self,email,id):
+        buckets = self.bucket_list_dictionaries.get(email)
+        bucket=buckets[id]
+        return bucket
