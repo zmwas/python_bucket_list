cc9cd846e8e3dbae8fb77173f5f65ce6c65a01e0
zac234 <zmwangimk@gmail.com>
2017-06-22 08:43:13 +0300

    Change folder structure

--- bucketlist/bucketlist/controllers/bucket_controller.py
+++ bucketlist/bucketlist/controllers/bucket_controller.py
@@ -1,6 +1,6 @@
-from user_controller import UserAuth
-from buckets.buckets import BucketList
 from buckets.bucket_items import BucketListItems
+from buckets.buckets import BucketList
+from controllers.user_controller import UserAuth
 
 
 class BucketController():
@@ -15,26 +15,22 @@ class BucketController():
 
         return self.bucket_list_dictionaries
 
-    def create_bucket_list(self,email ,name, completion_status):
+    def create_bucket_list(self, email, name, completion_status):
         bucketlist = BucketList()
+        buckets = self.bucket_list_dictionaries.get(email)
+        for bucket in buckets:
+            if name == bucket[0]:
+                return "Try another name for your bucket"
+
         bucketlist.create_bucket_list(name, completion_status)
-        buckets= self.bucket_list_dictionaries.get(email)
         buckets.append(bucketlist)
         return buckets
 
-
-    def add_bucket_item(self,email,bucket_name,name,completion_status):
+    def add_bucket_item(self, email, bucket_name, name, completion_status):
         bucket_item = BucketListItems()
-        bucket_item.create_bucket_items(name,completion_status)
-
-        buckets= self.bucket_list_dictionaries.get(email)
+        bucket_item.create_bucket_items(name, completion_status)
+        buckets = self.bucket_list_dictionaries.get(email)
         for bucket in buckets:
             if bucket_name == bucket[0]:
                 bucket.append(bucket_item)
-
-
                 return bucket
-
-
-
-
