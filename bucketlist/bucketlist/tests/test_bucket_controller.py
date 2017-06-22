449dea500c6debddaad2bc945f8e5bca6e8b1429
zac234 <zmwangimk@gmail.com>
2017-06-22 04:51:23 +0300

    Add test for add bucket item

--- bucketlist/bucketlist/tests/test_bucket_controller.py
+++ bucketlist/bucketlist/tests/test_bucket_controller.py
@@ -19,6 +19,7 @@ class BucketListTestCase(unittest.TestCase):
         self.buck = BucketController()
         self.auth = UserAuth()
 
+
     def test_create_bucket_directory(self):
         self.auth.add_user("Zack", "zacmwanginj@gmail.com", "password")
         lists = len(self.buck.create_bucket_directory())
@@ -27,7 +28,31 @@ class BucketListTestCase(unittest.TestCase):
 
     def test_create_bucket_list(self):
         self.auth.add_user("Zack", "johndoe@gmail.com", "password")
-        user= self.auth.list_of_users[0]
-        bucket= self.buck.create_bucket_list(user.email, "name", "status")[0]
 
-        self.assertEqual(len(bucket),2)
+        user = self.auth.list_of_users[0]
+        # The initial length of the list of bucket lists
+        initial = len(self.buck.bucket_list_dictionaries.get(user.email))
+        #create a bucket list
+        self.buck.create_bucket_list(user.email, "name", "status")
+        # The new length of the list after bucket list is created
+        new = len(self.buck.bucket_list_dictionaries.get(user.email))
+
+        self.assertEqual(initial + 1, new)
+
+
+
+    def test_add_bucket_item(self):
+        self.auth.add_user("Zack", "johndoe@gmail.com", "password")
+        self.buck.create_bucket_directory()
+        bucket = self.buck.create_bucket_list("johndoe@gmail.com", "new", "status")
+
+        print(bucket)
+        bucket_item = BucketListItems()
+
+        bucket_item.create_bucket_items("Make something")
+        created = self.buck.add_bucket_item("johndoe@gmail.com","new",bucket_item.name,bucket_item.completion_status)
+        print(created)
+        self.assertEqual(len(bucket),1)
+
+if __name__ == '__main__':
+    unittest.main()
