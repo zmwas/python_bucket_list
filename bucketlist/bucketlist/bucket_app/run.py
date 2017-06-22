a4ed522a9ecf2d511fb32b49b5714e1f37e65860
zac234 <zmwangimk@gmail.com>
2017-06-22 14:56:39 +0300

    Add view to register user

---
+++ bucketlist/bucketlist/bucket_app/run.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, request
+
+from bucket_app.forms import RegistrationForm, LoginForm, CreateBucketListForm
+from controllers.user_controller import UserAuth
+from controllers.bucket_controller import BucketController
+
+app = Flask(__name__)
+auth = UserAuth()
+bucket = BucketController()
+
+
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if auth.current_user.is_logged_in:
+        return redirect('/bucket')
+
+    form = RegistrationForm(csrf_enabled=False)
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        response = auth.add_user(name, email, password)
+        if type(response) == list:
+            return redirect('/login')
+            print(response)
+            print(name, password)
+        elif response == 'That email is taken':
+            redirect('/register')
+            print(response)
+
+    return render_template('registration.html', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
