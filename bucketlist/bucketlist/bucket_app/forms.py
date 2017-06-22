f9b61a49a97f61c9f246b406163e7ae3f3aae2af
zac234 <zmwangimk@gmail.com>
2017-06-22 14:48:26 +0300

    Add form for login a user

--- bucketlist/bucketlist/bucket_app/forms.py
+++ bucketlist/bucketlist/bucket_app/forms.py
@@ -8,3 +8,10 @@ class RegistrationForm(FlaskForm):
     email = StringField('email', validators=[Email()])
     password = PasswordField('password')
 
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[Email()])
+    password = PasswordField('password')
+
+
+
