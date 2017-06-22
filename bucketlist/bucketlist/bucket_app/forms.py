ae513dc3efdee173d5ff39c4b9cf936391ce6ab5
zac234 <zmwangimk@gmail.com>
2017-06-22 14:47:52 +0300

    Add form for registration

---
+++ bucketlist/bucketlist/bucket_app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField
+from wtforms.validators import Email
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('name')
+    email = StringField('email', validators=[Email()])
+    password = PasswordField('password')
+
