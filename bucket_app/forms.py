4846c463e6a0cf95e112723de4b74e379b5512cc
zac234 <zmwangimk@gmail.com>
2017-06-26 11:59:36 +0300

    Add forms to update bucket and bucket items

--- bucket_app/forms.py
+++ bucket_app/forms.py
@@ -1,22 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField
-from wtforms.validators import Email,InputRequired
+from wtforms.fields import StringField, PasswordField, SelectField
+from wtforms.validators import Email, InputRequired
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('name',validators=[InputRequired()])
+    name = StringField('name', validators=[InputRequired()])
     email = StringField('email', validators=[Email()])
     password = PasswordField('password')
 
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[Email()])
-    password = PasswordField('password',validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
 
 
 class CreateBucketListForm(FlaskForm):
-    bucket_name = StringField('Bucket Name ',validators=[InputRequired()])
+    bucket_name = StringField('Bucket Name ', validators=[InputRequired()])
 
 
 class CreateBucketItemForm(FlaskForm):
-    bucket_item_name = StringField('Bucket list item ',validators=[InputRequired()])
+    bucket_item_name = StringField('Bucket list item ', validators=[InputRequired()])
+
+
+class UpdateBucketListForm(FlaskForm):
+    bucket_name = StringField('Bucket Name ', validators=[InputRequired()])
+    completion_status = SelectField(u'Status', choices=[('Completed,Completed'), ('In Progress', 'In Progress'),
+                                                        ('On Ice', 'On Ice')])
+
+
+class UpdateBucketListForm(FlaskForm):
+    bucket_item_name = StringField('Bucket list item ', validators=[InputRequired()])
+    completion_status = SelectField(u'Status', choices=[('Completed,Completed'), ('In Progress', 'In Progress'),
+                                                        ('On Ice', 'On Ice')])
